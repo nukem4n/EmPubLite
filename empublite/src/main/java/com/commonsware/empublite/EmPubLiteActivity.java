b00b8d40905cb14abbf715020dacb71bfc18d52c
nukem4n <nukem4n@GMAIL.COM>
2014-02-11 19:08:12 -0500

    refactored project

--- empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,30 +1,20 @@
 package com.commonsware.empublite;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class EmPubLiteActivity extends ActionBarActivity {
+
+public class EmPubLiteActivity extends Activity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,20 +36,6 @@ public class EmPubLiteActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fr_main, container, false);
-            return rootView;
-        }
-    }
 
 }
