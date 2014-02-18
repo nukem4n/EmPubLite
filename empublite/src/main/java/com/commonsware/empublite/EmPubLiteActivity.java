6a6514183888d3481bcf2009009453180e0655e1
nukem4n <nukem4n@gmail.com>
2014-02-18 16:06:15 -0500

    re-factored action_settings to menu_settings

--- empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -30,7 +30,7 @@ public class EmPubLiteActivity extends SherlockFragmentActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
