890199236ed388f7c07748c8c6f78f9a587bf20a
nukem4n <nukem4n@GMAIL.COM>
2014-02-19 23:53:30 -0500

    added actionBarSherlock lib and setup menu items

--- empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,8 +1,9 @@
 package com.commonsware.empublite;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 
@@ -15,4 +16,23 @@ public class EmPubLiteActivity extends SherlockFragmentActivity {
         setContentView(R.layout.main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.options,menu);
+
+        return(super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                return (true);
+
+            case R.id.about:
+                return (true);
+        }
+    return(super.onOptionsItemSelected(item));
+
+  }
 }
