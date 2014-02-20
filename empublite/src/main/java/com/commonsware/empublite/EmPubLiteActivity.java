e9b9b5f5f299c99a7911246c27ccd0158d5d266b
nukem4n <nukem4n@GMAIL.COM>
2014-02-19 22:48:33 -0500

    fixed menu show action error

--- empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ empublite/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,7 +1,6 @@
 package com.commonsware.empublite;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -15,27 +14,5 @@ public class EmPubLiteActivity extends SherlockFragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.options, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.menu_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
 
 }
